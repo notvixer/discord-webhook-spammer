6e5b038df2c763cc9c9da6551cfb49e31b519d48
Vixer 99 <116427832+DefinitelyNotVixer@users.noreply.github.com>
2022-12-10 11:22:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ async def main():
     tasks = []
     for i in range(999):
       tasks.append(asyncio.create_task(spam(ses)))
-    await asyncio.gather(*tasks)
+   await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
   asyncio.run(main())
