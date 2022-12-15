9e8551f6ced2cce37e86843517046e3c1d80affc
Vixer 99 <116427832+DefinitelyNotVixer@users.noreply.github.com>
2022-12-15 22:52:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ message = "@here\nVixer Runs You <3" #enter your spam message here
 async def send(session):
   resp = await session.post(hook, json={"content":message})
   
-  if resp.status_code in (201,200,203):
+  if resp.status_code in (201,200,203, 204):
     print("Sent An Message Successfully")
   elif resp.status_code == 429:
     again = resp.headers["Retry-After"]
-    print("Ratelimited for %s seconds, Retrying " % (int(again)))
+    print("Ratelimited for %s milliseconds, Retrying " % (int(again)))
     await asyncio.sleep(int(again))
     await send(session)
 
