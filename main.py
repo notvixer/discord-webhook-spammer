bf2cf7cd4699135101d2635490036af0cd45848c
Vixer 99 <116427832+DefinitelyNotVixer@users.noreply.github.com>
2022-12-15 22:42:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,20 +17,21 @@ message = "@here\nVixer Runs You <3" #enter your spam message here
 ########################
 
 
-async def spam(session):
-  resp = await session.post(hook, json={'content': message})
-  if resp.status_code in (200, 201, 204):
-    print("Sent A Message Successfully")
+async def send(session):
+  resp = await session.post(hook, json={"content":message})
+  
+  if resp.status_code in (201,200,203):
+    print("Sent An Message Successfully")
   elif resp.status_code == 429:
-    again = await resp.json()
-    print('ratelimited for %d seconds, retrying...' % (again['retry_after']))
-    await asyncio.sleep(again['retry_after'])
-    await spam(session)
+    again = resp.headers["Retry-After"]
+    print("Ratelimited for %s seconds, Retrying " % (again))
+    await asyncio.sleep(int(again))
+    await send(session)
 
 
 async def main():
   async with aiosonic.HTTPClient() as ses:
-    await asyncio.gather(*[asyncio.create_task(spam(ses)) for i in range(999)])
+    await asyncio.gather(*[asyncio.create_task(send(ses)) for i in range(99)])
 
 if __name__ == '__main__':
   asyncio.run(main())
